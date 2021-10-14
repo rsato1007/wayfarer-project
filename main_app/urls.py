e33f2fade9eb661f3e0fd1fc61a3cc551268ed1f
Emily Johnson <emelizajo22@gmail.com>
2021-10-13 20:40:59 -0700

    changed file structure

--- main_app/urls.py
+++ main_app/urls.py
@@ -1,8 +1,6 @@
 from django.urls import path
 from . import views
 
-
 urlpatterns = [
-    path('', views.Home.as_view(), name="home")  ,
-    path('accounts/signup/', views.Signup.as_view(), name="signup"),
-]
+    path('', views.Home.as_view(), name="home"), 
+    ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,33 +1,7 @@
-from django.shortcuts import render, redirect
-from django.views import View
+from django.shortcuts import render
 from django.views.generic.base import TemplateView
 
-# at top of file with other imports
-from django.contrib.auth import login
-from django.contrib.auth.forms import UserCreationForm
-# Auth
-# from django.contrib.auth.decorators import login_required
-# from django.utils.decorators import method_decorator
 
 # Create your views here.
-
-
 class Home(TemplateView):
     template_name = "home.html"
-
-class Signup(View):
-    # show a form to fill out
-    def get(self, request):
-        form = UserCreationForm()
-        context = {"form": form}
-        return render(request, "registration/signup.html", context)
-    # on form submit validate the form and login the user.
-    def post(self, request):
-        form = UserCreationForm(request.POST)
-        if form.is_valid():
-            user = form.save()
-            login(request, user)
-            # return redirect("city_list")
-        else:
-            context = {"form": form}
-            return render(request, "registration/signup.html", context)

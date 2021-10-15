01f6f890082094b43b7fe4a3ba83deecfef7763e
nataliel2514 <natalielopez2514@gmail.com>
2021-10-14 23:31:19 -0500

    created profile page and user details

--- main_app/urls.py
+++ main_app/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.Home.as_view(), name="home"), 
-    path('registration/signup/', views.Signup.as_view(), name="signup")
+    path('registration/signup/', views.Signup.as_view(), name="signup"),
+    path('profile', views.ProfilePage.as_view(  ), name="profile"),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -3,6 +3,9 @@ from django.views.generic.base import TemplateView
 from django.views import View
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import login
+from .models import Profile
+
+from main_app.models import Profile
 
 # Create your views here.
 
@@ -26,3 +29,13 @@ class Signup(View):
         else:
             context = {"form": form}
             return render(request, "registration/signup.html", context)
+        
+    
+class ProfilePage(TemplateView):
+    model = Profile
+    template_name = "profile.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["profile"] = Profile.objects.all()
+        return context

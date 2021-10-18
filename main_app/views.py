851ef4bb58d7817b9b4e236de0e83856516f2745
Vincent Laquindanum <laq.vince@gmail.com>
2021-10-17 21:54:41 -0700

    fix views error

--- main_app/views.py
+++ main_app/views.py
@@ -24,7 +24,7 @@ class Home(TemplateView):
     template_name = "home.html"
 
     def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs
+        context = super().get_context_data(**kwargs)
         context["signupform"] = CustomUserCreationForm()
         context["loginform"] = AuthenticationForm()
         return context

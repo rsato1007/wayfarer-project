6f3ffcd26de8519659f23800b61c549d52a4dcb6
Danielle Brinkley <daniellebrinkley.fs@gmail.com>
2021-10-19 22:36:37 -0600

    add seed data

--- main_app/db/create_posts.sql
+++ main_app/db/create_posts.sql
@@ -2,7 +2,31 @@ INSERT INTO main_app_city(name, country, image)
     VALUES('LOS ANGELES, CA', 'US', 'null');
 
 INSERT INTO main_app_city(name, country, image)
+    VALUES('SAN FRANCISCO, CA', 'US', 'null');
+
+INSERT INTO main_app_city(name, country, image)
     VALUES('NEW YORK, NY', 'US', 'null');
 
 INSERT INTO main_app_city(name, country, image)
     VALUES('WASHINGTON, D.C', 'US', 'null');
+
+INSERT INTO main_app_city(name, country, image)
+    VALUES('BOSTON, MA' 'US', 'null');
+
+INSERT INTO main_app_city(name, country, image)
+    VALUES('SEATTLE, WA', 'US', 'null');
+
+INSERT INTO main_app_city(name, country, image)
+    VALUES('DENVER, CO', 'US', 'null');
+
+INSERT INTO main_app_city(name, country, image)
+    VALUES('NEW ORLEANS, LA', 'US', 'null');
+
+INSERT INTO main_app_city(name, country, image)
+    VALUES('MIAMI, FL', 'US', 'null');
+
+INSERT INTO main_app_city(name, country, image)
+    VALUES('ATLANTA, GA', 'US', 'null');
+
+INSERT INTO main_app_city(name, country, image)
+    VALUES('DETROIT, MI', 'US', 'null');

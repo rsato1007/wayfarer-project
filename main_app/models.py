b05a14abd76c516c4090c93fe88dc8fd3b785b2c
Emily Johnson <emelizajo22@gmail.com>
2021-10-23 15:44:03 -0700

    Make comment model

--- main_app/models.py
+++ main_app/models.py
@@ -53,3 +53,13 @@ class Post(models.Model):
     class Meta:
         ordering = ['title']
         
+
+class Comment(models.Model):
+    user = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    post_date = models.DateTimeField(auto_now_add=True)
+    title = models.CharField(max_length=100)
+    content = models.TextField(max_length=300)
+    parent_post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return f'{self.user} made comment {self.title} on {self.post_date}'

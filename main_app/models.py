437d8e20fe5f12a59680be1a21d008eee23063a4
Emily Johnson <emelizajo22@gmail.com>
2021-10-23 16:57:32 -0700

    Update comment model

--- main_app/models.py
+++ main_app/models.py
@@ -56,10 +56,10 @@ class Post(models.Model):
 
 class Comment(models.Model):
     user = models.ForeignKey(Profile, on_delete=models.CASCADE)
-    post_date = models.DateTimeField(auto_now_add=True)
     title = models.CharField(max_length=100)
     content = models.TextField(max_length=300)
+    created_date = models.DateTimeField(auto_now_add=True)
     parent_post = models.ForeignKey(Post, on_delete=models.CASCADE)
     
     def __str__(self):
-        return f'{self.user} made comment {self.title} on {self.post_date}'
+        return f'{self.user} made comment {self.title} on {self.created_date}'

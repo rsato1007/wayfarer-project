7bd513c352cca3922caede9216d81768c02c8a81
rsato1007 <robertmsato@gmail.com>
2021-10-25 20:28:39 -0600

    adding addtional css styling

--- main_app/static/scripts/main.js
+++ main_app/static/scripts/main.js
@@ -15,9 +15,9 @@ for (let i = 0; i < editForm.length; i++) {
 }
 
 // We'll use this code to eventually design a nav bar for mobile devices.
-if (screen.width <= 550) {
-    document.querySelector(".navbar").style.display = "none";
-} else {
-    document.querySelector(".hamburger-menu").style.display = "none";
-    document.querySelector(".mobile-header").style.display = "none";
-}
+// if (screen.width <= 550) {
+//     document.querySelector(".navbar").style.display = "none";
+// } else {
+//     document.querySelector(".hamburger-menu").style.display = "none";
+//     document.querySelector(".mobile-header").style.display = "none";
+// }
